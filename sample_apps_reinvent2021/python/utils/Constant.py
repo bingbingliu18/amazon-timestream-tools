dc91073618f37505a4cfbb263e41c5e80c28beab
Bingbing Liu <50776512+bingbingliu18@users.noreply.github.com>
2023-06-05 13:36:03 +0800

    Add files via upload

--- sample_apps_reinvent2021/python/utils/Constant.py
+++ sample_apps_reinvent2021/python/utils/Constant.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-DATABASE_NAME = "devops_multi"
-TABLE_NAME = "host_metrics"
+DATABASE_NAME = "stock"
+TABLE_NAME = "stock"
 SQ_RESULT_DATABASE = "sq_result_database_multi"
 SQ_RESULT_TABLE = "sq_result_table"
 HT_TTL_HOURS = 24
